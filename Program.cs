39af2b8da0976e6d474a43cf23a94468ef9dac67
pathakHemanta <pathakh@warhawks.ulm.edu>
2023-09-19 23:24:19 -0500

    .NET Application created and committed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstName;
+firstName = "Hemanta";
+
+var unknown = 3.78;
+
+// String Interpolation
+Console.WriteLine($"Hi! My name is {firstName}.");
+Console.WriteLine("What is the fraction of this number? : " + unknown);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is: " + DateTime.Now);
+
+Console.WriteLine("Is this \"legal\"?");
+
+Console.WriteLine("Windows " + 1 + 1);
